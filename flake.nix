a24cc03671ed266459cd1cf9ff99606ed32ef1a3
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-01-31 18:35:53 +0900

    add home, sway, git, foot

--- flake.nix
+++ flake.nix
@@ -3,14 +3,28 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11";
+
+    # home-manager, used for managing user configuration.
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ...  }: {
     nixosConfigurations.dqk = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.dqk = import ./home.nix;
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
       ];
     };
   };
---
+++ git.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Khanh Duong Quoc";
+    userEmail = "dqkqdlot@gmail.com";
+  };
+}
