7a3bf105b23ab5b334eee53803e0cbcc4cc004e6
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-02-01 08:57:17 +0900

    remove programs.nixvim inside nvim

--- home/nvim/plugins/none-ls.nix
+++ home/nvim/plugins/none-ls.nix
@@ -1,12 +1,10 @@
 {
-  programs.nixvim = {
-    plugins.none-ls = {
-      enable = true;
-      sources = {
-        formatting = {
-          nixfmt.enable = true;
-          stylua.enable = true;
-        };
+  plugins.none-ls = {
+    enable = true;
+    sources = {
+      formatting = {
+        nixfmt.enable = true;
+        stylua.enable = true;
       };
     };
   };
