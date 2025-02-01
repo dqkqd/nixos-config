7afa9cf905766218506cd2f2cb5a4d4c7cb39041
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-02-01 09:15:30 +0900

    add lazygit

--- home/nvim/keymaps.nix
+++ home/nvim/keymaps.nix
@@ -4,6 +4,14 @@
     {
       key = "<leader>fm";
       action.__raw = ''function() require("conform").format() end'';
+      options = { desc = "Format current buffer"; };
+    }
+
+    # lazygit
+    {
+      key = "<leader>gg";
+      action = ":LazyGit<CR>";
+      options = { desc = "Open lazygit gui"; };
     }
   ];
 
