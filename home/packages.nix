31fbb957bae9c0e3365017613cff2825d066dbce
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-02-01 15:47:47 +0900

    add more packages

--- home/packages.nix
+++ home/packages.nix
@@ -9,6 +9,13 @@
 
     just
 
+    # good utils for fzf in nvim
+    bat
+    delta
+    fd
+    ripgrep
+    skim
+
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
