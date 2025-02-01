4f2cef9851b088b066a560c74eb147effa408031
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-02-01 12:20:56 +0900

    add opts

---
+++ home/nvim/opts.nix
@@ -0,0 +1,29 @@
+{
+  opts = {
+    completeopt = "menu,menuone,noselect";
+    confirm = true;
+    cursorline = true;
+    expandtab = true;
+    ignorecase = true;
+    laststatus = 3;
+    linebreak = true;
+    list = true;
+    mouse = "a";
+    number = true;
+    relativenumber = true;
+    ruler = false;
+    shiftround = true;
+    shiftwidth = 2;
+    showmode = false;
+    smartcase = true;
+    smartindent = true;
+    smoothscroll = true;
+    spelllang = [ "en" ];
+    spell = true;
+    tabstop = 2;
+    termguicolors = true;
+    undofile = true;
+    undolevels = 10000;
+    updatetime = 200;
+  };
+}
