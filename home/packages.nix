7a0f1808090927da8c2a0b8ee5d0bccf3357ecc9
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-02-01 10:44:06 +0900

    separate packages

---
+++ home/packages.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+
+  home.packages = with pkgs; [
+    # utils
+    htop
+    fastfetch
+
+    # light
+    brightnessctl
+
+    just
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+}
