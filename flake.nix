b255769a16dd867e5e99f250d64dc7ce8201af31
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-02-01 09:31:22 +0900

    remove unneeded vars

--- flake.nix
+++ flake.nix
@@ -16,21 +16,19 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nixvim, ...  }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       legendary = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
-        modules = [./nixos/configuration.nix];
+        modules = [ ./nixos/configuration.nix ];
       };
     };
-    
+
     homeConfigurations = {
       dqk = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ./home/home.nix
-	];
+        modules = [ ./home/home.nix ];
       };
     };
   };
