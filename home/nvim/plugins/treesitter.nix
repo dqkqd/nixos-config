a0a0891d550918f2c138d60205bf007d689b6d29
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-02-01 11:01:59 +0900

    remove addtional syntax highlighting

--- home/nvim/plugins/treesitter.nix
+++ home/nvim/plugins/treesitter.nix
@@ -12,7 +12,7 @@
 
     settings = {
       auto_install = false;
-      highlight = { additional_vim_regix_highlighting = true; };
+      highlight = { additional_vim_regix_highlighting = false; };
       indent.enable = true;
     };
   };
