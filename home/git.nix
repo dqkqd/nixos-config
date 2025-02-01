046a943653b066ad4bc67a6605a57921022b2513
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-02-01 19:30:37 +0900

    config git

--- home/git.nix
+++ home/git.nix
@@ -1,11 +1,22 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
   programs.git = {
     enable = true;
-    userName = "Khanh Duong Quoc";
+    userName = "Khanh Duong";
     userEmail = "dqkqdlot@gmail.com";
+
+    delta.enable = true;
+
+    signing = {
+      key = "A10CE41E35455B76";
+      signByDefault = true;
+    };
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      pack = {
+        windowMemory = "2g";
+        packSizeLimit = "1g";
+      };
+    };
   };
 }
