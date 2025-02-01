2b1104cd49c8c345c0a865d4725d15d3c963a925
Khanh Duong Quoc <dqkqdlot@gmail.com>
2025-02-01 20:01:48 +0900

    restructure packages

--- home/packages.nix
+++ home/packages.nix
@@ -1,26 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    # utils
-    htop
-    fastfetch
-
-    # light
     brightnessctl
-
     just
-
-    # good utils for fzf in nvim
-    delta
-    fd
-    ripgrep
-    skim
-    fzf
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 }
--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,11 +1,20 @@
 {
   imports = [
-    ./bat.nix
     ./foot.nix
     ./git.nix
     ./nvim
     ./starship.nix
-    ./zoxide.nix
     ./zsh.nix
   ];
+
+  programs = {
+    bat.enable = true;
+    htop.enable = true;
+    fastfetch.enable = true;
+    fd.enable = true;
+    fzf.enable = true;
+    ripgrep.enable = true;
+    skim.enable = true;
+    zoxide.enable = true;
+  };
 }
