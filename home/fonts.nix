859437923bfe1b743ca864beed1a59f0be039aeb
Khanh Duong <dqkqdlot@gmail.com>
2025-02-03 06:48:46 +0900

    add more fonts

--- home/fonts.nix
+++ home/fonts.nix
@@ -1,5 +1,12 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [(nerdfonts.override {fonts = ["FiraCode" "Meslo"];})];
+  home.packages = with pkgs; [
+    noto-fonts
+    dejavu_fonts
+    roboto
+    font-awesome
+
+    (nerdfonts.override {fonts = ["FiraCode" "Meslo"];})
+  ];
 
   fonts.fontconfig.enable = true;
 }
