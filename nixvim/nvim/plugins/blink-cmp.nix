9eb498d768a94ac30bc8daec354b48043be86831
Khanh Duong <dqkqdlot@gmail.com>
2025-02-02 21:15:47 +0900

    add sources, configs for blink cmp

--- nixvim/nvim/plugins/blink-cmp.nix
+++ nixvim/nvim/plugins/blink-cmp.nix
@@ -3,17 +3,25 @@
     enable = true;
 
     settings = {
-      signature.enabled = true;
+      appearance = {
+        use_nvim_cmp_as_default = false;
+        nerd_font_variant = "normal";
+      };
 
       completion = {
+        accept.auto_brackets.enabled = true;
+
         ghost_text.enabled = true;
+
         documentation = {
           auto_show = true;
+          auto_show_delay_ms = 200;
         };
 
         menu = {
           enabled = true;
           auto_show = true;
+          draw.treesitter = ["lsp"];
         };
       };
 
@@ -29,6 +37,12 @@
           "select_prev"
           "fallback"
         ];
+      };
+
+      signature.enabled = true;
+      sources = {
+        cmdline = [];
+        default = ["lsp" "path" "snippets" "luasnip" "buffer"];
       };
     };
   };
