322d96fba3642554d169b8d68e48459241f9c4fc
Khanh Duong <dqkqdlot@gmail.com>
2025-02-03 21:02:10 +0900

    add direnv

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -17,5 +17,36 @@
         "zoxide"
       ];
     };
+
+    # init direnv: https://github.com/nix-community/nix-direnv/wiki/Shell-integration
+    initExtra = ''
+      nixify() {
+        if [ ! -e ./.envrc ]; then
+          echo "use nix" > .envrc
+          direnv allow
+        fi
+        if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+          cat > default.nix <<'EOF'
+      with import <nixpkgs> {};
+      mkShell {
+        nativeBuildInputs = [
+          bashInteractive
+        ];
+      }
+      EOF
+      ''\$EDITOR default.nix
+        fi
+      }
+
+      flakify() {
+        if [ ! -e flake.nix ]; then
+          nix flake new -t github:nix-community/nix-direnv .
+        elif [ ! -e .envrc ]; then
+          echo "use flake" > .envrc
+          direnv allow
+        fi
+      ''\$EDITOR flake.nix
+      }
+    '';
   };
 }
