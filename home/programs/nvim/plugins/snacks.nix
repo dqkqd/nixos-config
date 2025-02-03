6a16776cfe59c1b768b1a4bf5dd1ce384b22cdb0
Khanh Duong <dqkqdlot@gmail.com>
2025-02-03 16:21:54 +0900

    add snacks

---
+++ home/programs/nvim/plugins/snacks.nix
@@ -0,0 +1,25 @@
+{
+  plugins.snacks = {
+    enable = true;
+
+    settings = {
+      bigfile = {
+        enabled = true;
+      };
+      notifier = {
+        enabled = true;
+        timeout = 3000;
+      };
+      quickfile = {
+        enabled = false;
+      };
+      statuscolumn = {
+        enabled = false;
+      };
+      words = {
+        debounce = 100;
+        enabled = true;
+      };
+    };
+  };
+}
