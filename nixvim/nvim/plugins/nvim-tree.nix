93453a48d7cc990b467c370db79b590792078977
Khanh Duong <dqkqdlot@gmail.com>
2025-02-03 15:50:19 +0900

    add nvim-tree

---
+++ nixvim/nvim/plugins/nvim-tree.nix
@@ -0,0 +1,15 @@
+{
+  plugins.nvim-tree = {
+    enable = true;
+
+    diagnostics = {
+      enable = true;
+      showOnDirs = true;
+    };
+
+    view = {
+      number = true;
+      relativenumber = true;
+    };
+  };
+}
