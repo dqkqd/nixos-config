c3eef5ebcd9ab6c49a3d5c4e48111fcbfc677f27
Khanh Duong <dqkqdlot@gmail.com>
2025-02-03 15:50:19 +0900

    add nvim-tree

--- home/programs/nvim/keymaps.nix
+++ home/programs/nvim/keymaps.nix
@@ -4,20 +4,41 @@
     {
       key = "<leader>fm";
       action = ":Format<CR>";
-      options = { desc = "Format current buffer"; };
+      options = {desc = "Format current buffer";};
     }
     {
       key = "<leader>ft";
       action = ":FormatToggle<CR>";
-      options = { desc = "Toggle autoformat on-save"; };
+      options = {desc = "Toggle autoformat on-save";};
     }
 
     # lazygit
     {
       key = "<leader>gg";
       action = ":LazyGit<CR>";
-      options = { desc = "Open lazygit gui"; };
+      options = {desc = "Open lazygit gui";};
+    }
+
+    # nvim tree
+    {
+      key = "<leader>nt";
+      action = ":NvimTreeToggle<CR>";
+      options = {desc = "NvimTree Toggle";};
+    }
+    {
+      key = "<leader>nc";
+      action = ":NvimTreeClose<CR>";
+      options = {desc = "NvimTree Close";};
+    }
+    {
+      key = "<leader>nf";
+      action = ":NvimTreeFocus<CR>";
+      options = {desc = "NvimTree Focus";};
+    }
+    {
+      key = "<leader>nb";
+      action = ":wincmd p<CR>";
+      options = {desc = "NvimTree Focus Editor";};
     }
   ];
-
 }
