40b39b118a35a29973984ffeca2246ce192edb02
Khanh Duong <dqkqdlot@gmail.com>
2025-02-05 09:26:02 +0900

    add input method

--- users/default.nix
+++ users/default.nix
@@ -17,7 +17,23 @@
 
   # locale and timezone
   time.timeZone = "Asia/Tokyo";
-  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = ["en_US.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8"];
+    inputMethod = {
+      type = "fcitx5";
+      enable = true;
+      fcitx5.waylandFrontend = true;
+      fcitx5.addons = with pkgs; [
+        # japanese
+        fcitx5-mozc
+        # vietnamese
+        fcitx5-unikey
+        fcitx5-gtk
+      ];
+    };
+  };
 
   # https://nixos.wiki/wiki/Sway
   # enable polkit for setting up sway in home-manager
