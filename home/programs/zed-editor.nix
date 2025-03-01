acffe4fb1f9213aebff61276511250790f0ff803
Khanh Duong <dqkqdlot@gmail.com>
2025-03-01 17:13:16 +0900

    feat(zed): add more keybindings

--- home/programs/zed-editor.nix
+++ home/programs/zed-editor.nix
@@ -13,8 +13,15 @@
 
     userKeymaps = [
       {
+        bindings = {
+          # avoid  close everything
+          "ctrl-shift-w" = "pane::CloseActiveItem";
+        };
+      }
+      {
         context = "VimControl && !menu";
         bindings = {
+          # tabs
           "space 1" = ["pane::ActivateItem" 0];
           "space 2" = ["pane::ActivateItem" 1];
           "space 3" = ["pane::ActivateItem" 2];
@@ -24,6 +31,13 @@
           "space 7" = ["pane::ActivateItem" 6];
           "space 8" = ["pane::ActivateItem" 7];
           "space 9" = ["pane::ActivateItem" 8];
+          "[ b" = "pane::ActivatePrevItem";
+          "] b" = "pane::ActivateNextItem";
+          "[ B" = "pane::SwapItemLeft";
+          "] B" = "pane::SwapItemRight";
+          "space b c" = "pane::CloseActiveItem";
+          "space b l" = "pane::CloseItemsToTheRight";
+          "space b h" = "pane::CloseItemsToTheLeft";
 
           # navigation
           "space f f" = "file_finder::Toggle";
@@ -32,6 +46,26 @@
           # multiple cursor
           "ctrl-n" = "editor::AddSelectionBelow";
           "ctrl-p" = "editor::SelectNext";
+        };
+      }
+
+      # project settings
+      {
+        context = "Workspace";
+        bindings = {
+          "ctrl-shift-e" = "project_panel::ToggleFocus";
+        };
+      }
+      {
+        context = "ProjectPanel && not_editing";
+        bindings = {
+          "n" = "project_panel::NewFile";
+          "shift-n" = "project_panel::NewDirectory";
+          "x" = "project_panel::Cut";
+          "y" = "project_panel::Copy";
+          "p" = "project_panel::Paste";
+          "r" = "project_panel::Rename";
+          "d" = "project_panel::Delete";
         };
       }
     ];
