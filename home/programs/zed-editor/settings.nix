d38cf15ac8649bbdff41be48a71cd9c2d39e4795
Khanh Duong <dqkqdlot@gmail.com>
2025-03-07 21:13:54 +0900

    feat(zed): add more config for gopls

--- home/programs/zed-editor/settings.nix
+++ home/programs/zed-editor/settings.nix
@@ -108,6 +108,38 @@
         binary = {
           path_lookup = true;
         };
+        initialization_options = {
+          gofumpt = true;
+          codelenses = {
+            gc_details = false;
+            generate = true;
+            regenerate_cgo = true;
+            run_govulncheck = true;
+            test = true;
+            tidy = true;
+            upgrade_dependency = true;
+            vendor = true;
+          };
+          hints = {
+            assignVariableTypes = true;
+            compositeLiteralFields = true;
+            compositeLiteralTypes = true;
+            constantValues = true;
+            functionTypeParameters = true;
+            parameterNames = true;
+            rangeVariableTypes = true;
+          };
+          analyses = {
+            nilness = true;
+            unusedparams = true;
+            unusedwrite = true;
+            useany = true;
+          };
+          usePlaceholders = true;
+          completeUnimported = true;
+          staticcheck = true;
+          semanticTokens = true;
+        };
       };
 
       json-language-server = {
