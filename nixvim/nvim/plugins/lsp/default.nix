2f878e33f7f9293fa1eb3c57a4c82d38e11abdac
Khanh Duong <dqkqdlot@gmail.com>
2025-03-23 05:32:43 +0900

    feat(nvim): golang

--- nixvim/nvim/plugins/lsp/default.nix
+++ nixvim/nvim/plugins/lsp/default.nix
@@ -14,6 +14,50 @@
           };
         };
       };
+
+      gopls = {
+        enable = true;
+        settings = {
+          gopls = {
+            # build
+            directoryFilters = ["-.direnv" "-.git" "-node_modules"];
+
+            # formatting
+            gofumpt = true;
+
+            # ui
+            codelenses = {
+              gc_details = false;
+              generate = true;
+              regenerate_cgo = true;
+              run_govulncheck = true;
+              test = true;
+              tidy = true;
+              upgrade_dependency = true;
+              vendor = true;
+            };
+            semanticTokens = true;
+
+            # comletion
+            usePlaceholders = true;
+            completeUnimported = true;
+
+            # diagnostic
+            staticcheck = true;
+
+            # inlayhint
+            hints = {
+              assignVariableTypes = true;
+              compositeLiteralFields = true;
+              compositeLiteralTypes = true;
+              constantValues = true;
+              functionTypeParameters = true;
+              parameterNames = true;
+              rangeVariableTypes = true;
+            };
+          };
+        };
+      };
     };
   };
 }
