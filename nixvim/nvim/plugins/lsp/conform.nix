f17d1e5961906802ff3c15abd52e031259be972b
Khanh Duong <dqkqdlot@gmail.com>
2025-04-04 23:07:02 +0900

    feat(nvim): allow to disable format

--- nixvim/nvim/plugins/lsp/conform.nix
+++ nixvim/nvim/plugins/lsp/conform.nix
@@ -13,6 +13,10 @@
       event = ["BufWritePre"];
     };
 
+    luaConfig.pre = ''
+      local slow_format_filetypes = {}
+    '';
+
     settings = {
       formatters_by_ft = {
         "just" = ["just"];
@@ -27,11 +31,6 @@
         lsp_format = "fallback";
       };
 
-      format_on_save = {
-        lsp_format = "fallback";
-        timeout_ms = 500;
-      };
-
       log_level = "off";
       notify_on_error = true;
       notify_no_formatters = true;
@@ -39,6 +38,44 @@
       formatters = {
         prettierd.command = lib.getExe pkgs.prettierd;
       };
+
+      format_on_save =
+        # Lua
+        ''
+          function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
+            end
+
+            if slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
+
+            local function on_format(err)
+              if err and err:match("timeout$") then
+                slow_format_filetypes[vim.bo[bufnr].filetype] = true
+              end
+            end
+
+            return { timeout_ms = 200, lsp_fallback = true }, on_format
+           end
+        '';
+
+      format_after_save =
+        # Lua
+        ''
+          function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
+            end
+
+            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
+
+            return { lsp_fallback = true }
+          end
+        '';
     };
   };
 }
