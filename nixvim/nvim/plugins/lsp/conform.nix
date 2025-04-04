09322836f061aa5fb4b50f0202a604976f3e5485
Khanh Duong <dqkqdlot@gmail.com>
2025-04-04 21:55:10 +0900

    feat(nvim): enable more formatters

--- nixvim/nvim/plugins/lsp/conform.nix
+++ nixvim/nvim/plugins/lsp/conform.nix
@@ -14,9 +14,19 @@
     };
 
     settings = {
+      formatters_by_ft = {
+        "just" = ["just"];
+        "*" = ["codespell"];
+        "_" = [
+          "trim_whitespace"
+          "trim_newlines"
+        ];
+      };
+
       default_format_opts = {
         lsp_format = "fallback";
       };
+
       format_on_save = {
         lsp_format = "fallback";
         timeout_ms = 500;
