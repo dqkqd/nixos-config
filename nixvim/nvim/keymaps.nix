1261594103ede75058ab4e8c165230c5915ee8e9
Khanh Duong <dqkqdlot@gmail.com>
2025-04-11 21:42:06 +0900

    feat(nvim): add flash

--- nixvim/nvim/keymaps.nix
+++ nixvim/nvim/keymaps.nix
@@ -68,5 +68,12 @@
 
     # search and replace
     { mode = ["n" "v" ]; key = "<leader>sr"; action = "<cmd>GrugFar<cr>"; options = { desc = "Search and Replace"; }; }
+
+    # flash
+    { mode = ["n" "x" "o" ]; key = "s"; action.__raw = "function() require'flash'.jump() end"; options = { desc = "Flash"; }; }
+    { mode = ["n" "x" "o" ]; key = "S"; action.__raw = "function() require'flash'.treesitter() end"; options = { desc = "Flash Treesitter"; }; }
+    { mode = "o"; key = "r"; action.__raw = "function() require'flash'.remote() end"; options = { desc = "Remote Flash"; }; }
+    { mode = ["o" "x"]; key = "R"; action.__raw = "function() require'flash'.treesitter_search() end"; options = { desc = "Treesitter Search"; }; }
+    { mode = "c"; key = "<c-s>"; action.__raw = "function() require'flash'.toggle() end"; options = { desc = "Toggle Flash Search"; }; }
   ];
 }
